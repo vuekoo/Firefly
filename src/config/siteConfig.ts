c1a0d4f87b3d92001928f7d6037253f8b98c0a5e
vuekoo <2710155768@qq.com>
2026-06-29 10:26:42 +0800

    update

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -62,7 +62,7 @@ export const siteConfig: SiteConfig = {
 	favicon: [
 		{
 			// 图标文件路径
-			src: "/favicon/favicon.ico",
+			src: "public/favicon/favicon.ico",
 			// 可选，指定主题 'light' | 'dark'
 			// theme: "light",
 			// 可选，图标大小
